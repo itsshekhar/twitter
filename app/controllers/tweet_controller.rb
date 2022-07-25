46cf33ba0b7a4454628085e3c4cce271baba6698
shekhar <official.shekhar@hotmail.com>
2022-07-25 18:55:28 +0530

    first page created

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,23 @@
+class TweetController < ApplicationController
+    def index
+        @tweet=Tweet.all
+    end
+    
+    def new
+
+    end
+    
+    def edit
+
+    end
+    
+    def create
+
+    end
+    
+    def destroy
+        
+    end
+   
+
+end
