19b9f270128fee1a1294fb3e237306724a370400
shekhar <official.shekhar@hotmail.com>
2022-07-26 19:02:40 +0530

    create tweet fuctionality added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,28 +1,57 @@
 class TweetsController < ApplicationController
+
     def index
-        @tweets=Tweet.all
+        @tweets=Tweet.all.order("created_at DESC")
         @tweet=Tweet.new()
 
     end
     
-    def new
-        @tweet=Tweet.new
+     
+    def show
+        @tweet = Tweet.find(params[:id])
+   end
 
+   def new
+         @tweet=Tweet.new()
+   end
+
+   def create
+    p "-------------------------------"
+    p tweet_params
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id=1
+         if @tweet.save
+         redirect_to root_path
+         else
+         render :new, status: :unprocessable_entity
+         end
     end
-    
+
     def edit
-
+         @tweet=Tweet.find(params[:id])
     end
-    
-    def create
 
-    end
-    
+    def update
+         @tweet = Tweet.find(params[:id])
+         puts params
+         puts"-------------------------------"
+         if @tweet.update(tweet_params)
+           redirect_to tweet_url(@tweet[:id])
+         else
+           render :edit, status: :unprocessable_entity
+         end
+    end  
+
     def destroy
-        
+         @tweet = Tweet.find(params[:id])
+         @tweet.destroy
+         redirect_to tweet_index_path, status: :see_other
     end
-   
-
-end
+         
+    private
+    def tweet_params  
+        p"========================"
+         params.require(:tweet).permit(:body)
+   end
 
 end
